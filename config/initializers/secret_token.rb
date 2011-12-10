02722d3d50ca1463a4f0ce7319abf2bc22c746b5
slamice <slamice@gmail.com>
2011-12-11 00:28:16 +0900

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdrusCom::Application.config.secret_token = '47c709c739c3ad88257261763555f0f2dc53ae79da25a9ffd1bb97171943500a3038d82a11680d17f24afba2ff94f59b021728a86fc1a0ee05bf17e608c3546a'
